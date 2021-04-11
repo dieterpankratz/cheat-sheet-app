30dc872f40da88a3fcb3cf81fea40f461f480a20
Dieter Pankratz <pankratz117@gmail.com>
2021-04-11 16:44:26 +0200

    warning fixup

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -72,7 +71,6 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
       {/* prevent direct access between iFrame and parent document with sandbox="":
       (downside: inside of the context of the iFrame you cannot use localStorage
       and a couple other borwser features! ) */}
