cbe13cd84ad8d591960683b9eb1e2b35b8e0afc5
Dieter Pankratz <pankratz117@gmail.com>
2021-04-17 15:52:57 +0200

    add some more monaco config

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -6,7 +6,14 @@ const CodeEditor = () => {
       language="javascript"
       theme="dark" 
       options={{
-        wordWrap: "on"
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }} 
       height="500px" 
     />)
