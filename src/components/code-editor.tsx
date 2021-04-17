b4f11ccc1ee57147b2c6d77abe6e54f23bfbc251
Dieter Pankratz <pankratz117@gmail.com>
2021-04-17 17:31:13 +0200

    add automatic code formatting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,8 @@
+import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,7 +10,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>();
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
@@ -14,7 +21,24 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     monacoEditor.getModel()?.updateOptions({ tabSize: 2});
   }
   
+  const onFormatClick = () => {
+    // get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    });
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
+  }
+  
   return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
     <MonacoEditor
       editorDidMount={onEditorDidMount}
       value={initialValue}  // monaco's value is just the initial value!
@@ -31,7 +55,9 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         automaticLayout: true,
       }} 
       height="500px" 
-    />)
+    />
+    </div>
+    )
 }
 
 export default CodeEditor;
