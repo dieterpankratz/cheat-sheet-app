dd63726343230162af587cd84938552053da56c0
Dieter Pankratz <pankratz117@gmail.com>
2021-04-17 15:58:22 +0200

    set initial value

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,13 @@
 import MonacoEditor from '@monaco-editor/react'
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
   return (
     <MonacoEditor 
+      value={initialValue}  // monaco's value is just the initial value!
       language="javascript"
       theme="dark" 
       options={{
