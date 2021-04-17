6e70f2d1b1559621c6eb3861b50b4a67ec7d73d5
Dieter Pankratz <pankratz117@gmail.com>
2021-04-17 15:44:59 +0200

    add some configuration to monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,15 @@
 import MonacoEditor from '@monaco-editor/react'
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />
+  return (
+    <MonacoEditor 
+      language="javascript"
+      theme="dark" 
+      options={{
+        wordWrap: "on"
+      }} 
+      height="500px" 
+    />)
 }
 
 export default CodeEditor;
