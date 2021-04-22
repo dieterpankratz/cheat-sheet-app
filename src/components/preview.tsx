7a0beeed865e0885fbd089caf28caee76ae50a1d
Dieter Pankratz <pankratz117@gmail.com>
2021-04-22 19:09:08 +0200

    extract reusable error handling logic

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -13,13 +13,16 @@ const html = `
     <body>
       <div id="root"></div>
       <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><div>😱🤬😷</div> <h4>Runtime Error</h4>' + err + '<div>🦆🧐📚</div></div>'
+        console.error(err);
+      };
         window.addEventListener('message', (e) => {
           try {
             eval(event.data);
           } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><div>😱🤬😷</div> <h4>Runtime Error</h4>' + err + '<div>🦆🧐📚</div></div>'
-            console.error(err);
+            handleError(err);
           }
         }, false)
       </script>
